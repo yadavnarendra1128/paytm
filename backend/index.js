462e688ebcd457a24c3425716b93e0267fb73784
Narendra1128 <yadavnarendra1128@gmail.com>
2024-12-18 17:29:00 +0530

    checkpoint

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const cors = require("cors")
-const app =express()
 const {users} = require("./db")
 const mainRouter = require("./routes/index");
+const app = express();
 
+// request from other servers like frontend
 app.use(cors())
+// request body json  parser middleware
 app.use(express.json())
 
 app.use("app/v1",mainRouter);
-app.listen(3000)
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000") 
+})
 
